f93e0f27cd6916970c0f6ee692b6a42bf6d3f4fa
LeticiaGenadeze <lety_genadeze@hotmail.com>
2015-03-07 15:52:57 -0300

    Classe Main
    Criação da Classe main

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main{
+	public static void main (String args[]){
+		Retangulo r;
+		r = new Retangulo();
+		r.base = 15;
+		r.altura = 10;
+		r.Calcula_Area();
+		r.Calcula_Perimetro();
+	}
+}
